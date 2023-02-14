966badbb728da38492bdf810b549a646839edb15
SumiShilpa <113533159+SumiShilpa@users.noreply.github.com>
2023-02-14 10:53:32 -0500

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Software_Development_Project.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
